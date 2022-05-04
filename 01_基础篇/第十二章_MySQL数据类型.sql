84c3293a2c42e0b2a7d64eef46f16a105b7b77a5
Mrzhi <zhilx1997@sina.com>
2022-05-04 10:29:27 +0800

    添加关于mysql数据库的关于整形、浮点数、定点数与位类型的说明和测试

--- 01_基础篇/第十二章_MySQL数据类型.sql
+++ 01_基础篇/第十二章_MySQL数据类型.sql
@@ -142,4 +142,213 @@ CREATE TABLE test_int3(
 DESC test_int3;
 
 
+## 3. 浮点类型
+/* 3.1 类型介绍：
+		
+		浮点数和定点数类型的特点是可以处理小数，我们可以把整数看成小数的一个特例。
+	  因此，浮点数和定点数的是使用场景相比于整数则多了很多。MySQL支持的浮点数类型：
+		分别是FOLAT，DOUBLE，REAL
 
+			- FLOAT表示单精度浮点数（4个字节）
+			- DOUBLE表示双精度浮点数（8个字节）
+
+		  - REAL 默认就是DOUBLE。如果把SQL模式设定为启用“REAL_AS_FLOAT”，那么，MySQL就
+				认为REAL是FLOAT。如果要启用“REAL_AS_FLAOT”，可以通过以下的SQL语句实现：
+
+				SET sql_mode = "REAL_AS_FLOAT";
+
+		问题1：FLOAT和DOUBLE类型的区别？
+		问题2：为什么浮点数类型的无符号取值范围，只有相当于有符号取值范围的一半，也就是只相当于有符号数
+					 取值范围大于零的部分呢？
+
+					 MySQL存储浮点数的格式为：符号(S), 尾数(M)和阶码(E)。因此，无论有没有符号，mysql的浮点数都
+					 会存储表示符号的部门。因此，所谓的无符号数取值范围，其实就有有符号数取值范围大于等于零的部门。
+
+		3.2 精度误差说明
+			
+				浮点数类型有个缺陷，就是不准确。
+
+				这里我们解释一下为什么MySQL的浮点数不够精确。比如，我们设计一个表，有price这个字段，插入值分别为
+				0.47, 0.44, 0.19，我们期待的运行结果是：0.47 + 0.44 + 0.19 = 1.1。而是用SUM查询的结果为：1.09999999999
+				如果将数据类型改为float，则得到的结果是1.099999940395355, 可以发现误差更大了。
+
+				存着这样误差的原因在于：mysql对浮点数据的存储方式上。
+				MySQL 用4个字节存储FLOAT类型数据，用8个字节来存储DOUBLE类型数据。无论那个，都是采用二进制的方式来进行存储的。
+				比如9.625，用二进制来表达，就是1001.101，或者表达成1.001101*2^3。如果尾数不是0或5（比如9.624），我们就无法用
+				一个二进制数来精确表达。进而，就只好在取值范围允许的范围内进行四舍五入。
+
+				在编程中，如果用到浮点数，要特别主要误差问题，因为浮点数是不准确的，所有我们要避免使用’=‘来判断两个数是否相等。
+				同时，在一些对精度要求比较高的项目中，千万不要使用浮点数，不然会导致结果错误，甚至造成不可挽回的损失。那么，
+				Mysql有没有精准的数据类型呢？当然有，就是定点数类型：DECIMAL。
+				
+ */
+
+CREATE TABLE test_double1(
+	f1 float,
+	f2 float(5, 2),
+	f3 double,
+	f4 double(5,2)
+);
+
+DESC test_double1;
+
+INSERT INTO test_double1(f1, f2) VALUES (123.45, 123.45);
+
+INSERT INTO test_double1(f3, f4) VALUES (123.45, 123.456);
+INSERT INTO test_double1(f3, f4) VALUES (999.99, 999.989);
+INSERT INTO test_double1(f3, f4) VALUES (1234.456, 999.99);
+INSERT INTO test_double1(f3, f4) VALUES (1234.456, 999.999); # [Err] 1264 - Out of range value for column 'f4' at row 1
+INSERT INTO test_double1(f3, f4) VALUES (1234.456, 123.456); # [SQL] # [Err] 1264 - Out of range value for column 'f4' at row 1
+
+SELECT * FROM test_double1;
+
+
+# 3.3 精度损失问题
+# 测试float和double的精度
+CREATE TABLE test_double2(
+f1 DOUBLE
+);
+
+INSERT INTO test_double2 VALUES(0.47),(0.44),(0.19);
+
+SELECT SUM(f1) FROM test_double2; # 1.0999999999999999
+
+SELECT SUM(f1) = 1.1, 1.1 = 1.1 FROM test_double2;
+
+
+## 4. 定点数类型
+/* 类型介绍：
+	 1) MySQL中的定点数类型只有DECIMAL一种类型
+	
+	 数据类型											字节数				含义
+
+	 DECIMAL(M,D), DEC, NUMERIC		M+2字节				有效范围由M和D决定
+
+	 使用DECIMAL(M,D)的方式表示高精度小数。其中，M被称为精度，D被称为标度。
+	 0<=M<=65, 0<=D<=30, D < M。例如，定义DECIMAL(5,2)，表示该列取值范围是-999.99 ~ 999.99
+
+	 2) DECIMAL(M,D) 的最大取值范围和DOUBLE类型一样，但是有效的数据范围是由M和D决定的。DECIMAL的存储空间并不是固定的，
+			由精度值M决定，总共占用的存储空间为M+2个字节。也就是说，在一些对精度要求不高的场景下，比起占用同样字节长度的
+			定点数，返点数表达的数值范围可以更大一些。
+	
+	 3) 定点数在MySQL内部是以 字符串 的形式进行存储，这就决定了它一定是精确的。
+
+	 4) 当DECIMAL类型不指定精度和标度时，其默认为DEICMAL(10, 0)。当数据的精度超出了定点数类型的精度范围时，则MySQL
+			同样会进行四舍五入处理。
+
+	 5) 浮点数 vs 定点数
+			
+			* 浮点数相对于定点数的有点是在长度一定的情况下，浮点类型取值范围大，但是不精准，适用于需要取值范围大，
+			  又可以容忍微小误差的科学计算场景（比如计算化学、分子建模、流体动力系等）
+
+			* 定点数类型取值范围相对小，但是精准，没有误差，适用于对精度要求极高的场景（比如设计金额计算的场景）
+
+ */
+
+CREATE TABLE test_decimal1(
+	f1 DECIMAL,
+	f2 DECIMAL(5,2)
+);
+
+DESC test_decimal1;
+
+# 此时存在四舍五入的情况
+INSERT INTO test_decimal1(f1, f2) VALUES(123.123, 123.456);
+
+SELECT * FROM test_decimal1;
+
+# Out of range value for column 'f2' at row 1
+INSERT INTO test_decimal1(f2) VALUES(1234.34);
+ 
+INSERT INTO test_decimal1(f2) VALUES(999.99); 
+INSERT INTO test_decimal1(f2) VALUES(999.995); 
+
+# 举例：把test_double2表中的f1字段的数据类型修改为decimal(5,2)
+ALTER TABLE test_double2 MODIFY f1 DECIMAL(5,2);
+
+DESC test_double2;
+
+SELECT SUM(f1) FROM test_double2; # 1.10
+
+SELECT SUM(f1) = 1.1, 1.1 = 1.1 FROM test_double2; # 1 1
+ 
+
+
+## 5. 位类型：BIT
+/* BIT类型中存储的是二进制值，类似010110。
+
+	 二进制字符串类型		长度		长度范围			占用空间
+	 BIT(M) 						M	 			1 <= M <= 64 	约为(M + 7)/8个字节
+ 
+	 BIT类型，如果没有指定(M)，默认是1位。这个1位，表示只能存1位的二进制值。这里(M)是表示二进制的位数，位数最小值为1，最大值为64。
+
+
+	# 可以使用BIN来查看二进制, HEX查看十六进制
+	SELECT BIN(f1), BIN(f2), BIN(f3), HEX(f1), HEX(f2), HEX(f3), FROM test_bit1;
+
+	# 显示十进制：通过加0，可以以十进制的方式显示数据
+	SELECT f1 + 0, f2 + 0, f3 + 0 FROM test_bit1;
+ */
+CREATE TABLE test_bit1(
+	f1 BIT,
+  f2 BIT(5),
+	f3 BIT(64)
+);
+
+DESC test_bit1;
+
+-- CREATE TABLE test_bit2(
+-- 	f1 BIT,
+--   f2 BIT(5),
+-- 	f3 BIT(65) # [Err] 1439 - Display width out of range for column 'f3' (max = 64)
+-- );
+
+INSERT INTO test_bit1(f1) VALUES (0), (1);
+INSERT INTO test_bit1(f1) VALUES (01), (01);
+INSERT INTO test_bit1(f1) VALUES (10); # [Err] 1406 - Data too long for column 'f1' at row 1
+
+INSERT INTO test_bit1(f1, f2, f3) VALUES (1, 1001, 111111000000111111);
+
+SELECT * FROM test_bit1;
+/*
+mysql> SELECT * FROM test_bit1;
++------------+------------+------------+
+| f1         | f2         | f3         |
++------------+------------+------------+
+| 0x00       | NULL       | NULL       |
+| 0x01       | NULL       | NULL       |
+| 0x01       | NULL       | NULL       |
+| 0x01       | NULL       | NULL       |
++------------+------------+------------+
+4 rows in set (0.00 sec)
+*/
+
+INSERT INTO test_bit1(f1) VALUES (2); # [Err] 1406 - Data too long for column 'f1' at row 1
+
+INSERT INTO test_bit1(f2) VALUES (2); # right
+INSERT INTO test_bit1(f2) VALUES (31); # 11111: 最大是31
+SELECT * FROM test_bit1;
+
+INSERT INTO test_bit1(f3) VALUES (123891);
+/*
+mysql> SELECT * FROM test_bit1;
++------------+------------+--------------------+
+| f1         | f2         | f3                 |
++------------+------------+--------------------+
+| 0x00       | NULL       | NULL               |
+| 0x01       | NULL       | NULL               |
+| 0x01       | NULL       | NULL               |
+| 0x01       | NULL       | NULL               |
+| NULL       | 0x02       | NULL               |
+| NULL       | 0x1F       | NULL               |
+| NULL       | NULL       | 0x000000000001E3F3 |
++------------+------------+--------------------+
+7 rows in set (0.00 sec)
+*/
+
+
+# 可以使用BIN来查看二进制, HEX查看十六进制
+SELECT BIN(f1), BIN(f2), BIN(f3), HEX(f1), HEX(f2), HEX(f3), FROM test_bit1;
+
+# 显示十进制：通过加0，可以以十进制的方式显示数据
+SELECT f1 + 0, f2 + 0, f3 + 0 FROM test_bit1;
