5a46a635cab8f1cf78f4eaab2b029c6395b9ca34
Mrzhi <zhilx1997@sina.com>
2022-05-09 11:39:00 +0800

    添加对存储函数的创建和调用的代码测试，以及与存储过程进行了对比

--- 01_基础篇/第十五章_存储过程与函数.sql
+++ 01_基础篇/第十五章_存储过程与函数.sql
@@ -51,6 +51,52 @@
 			修饰符 返回类型 方法名(参数类型 参数名, ...) {
 					方法体;
 			}
+
+3. 如何调试
+	 在mysql中，存储过程不像普通的编程语言（比如VC++,java等）那样有专门的集成开发环境。
+	 因此，我们可以通过SELECT语句，把程序执行的中间结果查询出来，来调试一个SQL语句的正确性。
+	 调试成功之后，把SELECT语句后移到下一个SQL语句之后，再调试下一个SQL语句。这样`逐步推进`，
+	 就可以完成对存储过程中所有操作的调试了。当然，我们可以把存储过程中的SQL语句复制出来，
+	 逐段单独调试。
+
+4. 存储函数的使用：
+	 前面学习了很多函数，使用这些函数可以对数据进行各种处理操作，极大地提高用户对数据库的管理效率。
+	 MySQL支持自定义函数，定义好之后，调用方式与调用MySQL预定义的系统函数一样。
+
+	 
+	 4.1 语法分析
+		 学过的函数：LENGTH、SUBSTR、CONCAT等
+		 语法格式：
+				CREATE FUNCTION 函数名(参数名 参数类型, ...)
+				RETURNS 返回值类型
+				[characteristics ...]	
+				BEGIN	
+						函数体 # 函数体中肯定有RETURN语句
+				END
+			
+
+			说明：
+				1) 参数列表：指定参数IN、out或INOUT只对procedure是合法的，FUNCTION中总是默认为IN参数
+				2) RETURNS TYPE 语句表示函数返回数据的类型
+					 RETURNs子句只能对FUNCTION做指定，对函数而言这是强制的。它用来指定函数的返回类型，而且函数
+					 体必须包含一个RETURN value语句。
+				3) characteristic 创建函数时指定的对函数的约束。取值与创建存储过程相同。
+				4) 函数体也可以用BEGIN ... END来表示SQL代码的开始和结束。如果函数体只有一条语句，也可以省略BEGIN ... END.
+
+		4.2 调用存储函数
+			  在MySQL中，存储函数的使用方法与MySQL内部函数的使用方法是一样的。换言之，用户自己定义的存
+				储函数与MySQL内部函数是一个性质的。区别在于，存储函数是用户自己定义的，而内部函数是MySQL
+				的开发者定义的。
+
+				SELECT 函数名(实参列表)
+
+5. 对比存储过程和存储函数
+								关键字				调用语法					返回值							应用场景
+		存储过程    PROCEDURE 		CALL 存储过程()		理解为有0个或多个		一般用于更新
+		存储函数		FUNCTION  		SELECT 函数() 		只能是一个      		一般用于查询结果为一个值并返回时
+
+		此外，存储函数可以放在查询语句中使用，存储过程不行。反之，存储过程的功能更加强大，
+		包括能够执行对表的操作（比如创建表，删除表等）和事物操作，这些功能的存储函数是不具备的。
  */
 # 0. 准备工作
 CREATE DATABASE dbtest15;
@@ -178,10 +224,102 @@ CALL show_mgr_name(@empname);
 SELECT @empname;
 
 
+/* 
+4. 存储函数的使用：
+	 前面学习了很多函数，使用这些函数可以对数据进行各种处理操作，极大地提高用户对数据库的管理效率。
+	 MySQL支持自定义函数，定义好之后，调用方式与调用MySQL预定义的系统函数一样。
+
+	 
+	 4.1 语法分析
+		 学过的函数：LENGTH、SUBSTR、CONCAT等
+		 语法格式：
+				CREATE FUNCTION 函数名(参数名 参数类型, ...)
+				RETURNS 返回值类型
+				[characteristics ...]	
+				BEGIN	
+						函数体 # 函数体中肯定有RETURN语句
+				END
+			
+
+			说明：
+				1) 参数列表：指定参数IN、out或INOUT只对procedure是合法的，FUNCTION中总是默认为IN参数
+				2) RETURNS TYPE 语句表示函数返回数据的类型
+					 RETURNs子句只能对FUNCTION做指定，对函数而言这是强制的。它用来指定函数的返回类型，而且函数
+					 体必须包含一个RETURN value语句。
+				3) characteristic 创建函数时指定的对函数的约束。取值与创建存储过程相同。
+				4) 函数体也可以用BEGIN ... END来表示SQL代码的开始和结束。如果函数体只有一条语句，也可以省略BEGIN ... END.
+
+		4.2 调用存储函数
+			  在MySQL中，存储函数的使用方法与MySQL内部函数的使用方法是一样的。换言之，用户自己定义的存
+				储函数与MySQL内部函数是一个性质的。区别在于，存储函数是用户自己定义的，而内部函数是MySQL
+				的开发者定义的。
+
+				SELECT 函数名(实参列表)
+
+		4.3 注意：
+				若在创建存储函数中报错"you might want to use the less safe log_bin_trust_function_creators variable"，有两种处理方法：
+				1) 加上必要的函数特性"[NOT] DETERMINISTIC" 和 "{CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA}"
+				2) mysql> SET GLOBAL log_bin_trust_function_creators = 1;
+ */
+## 3. 存储函数
+# 举例1：创建存储函数，名称为email_by_name()，参数定义为空，该函数查询Abel的email，并返回，数据类型为字符串类型。
+# 1) 加上必要的函数特性"[NOT] DETERMINISTIC" 和 "{CONTAINS SQL | NO SQL | READS SQL DATA | MODIFIES SQL DATA}"
+DELIMITER //
+
+CREATE FUNCTION email_by_name()
+RETURNS VARCHAR(25)
+			  DETERMINISTIC
+				CONTAINS SQL
+				READS SQL DATA
+BEGIN
+	RETURN (SELECT email FROM employees WHERE last_name = 'Abel');
+
+END //
+
+DELIMITER ;
+
+# 调用
+SELECT email_by_name();
+
+# 举例2：创建存储函数，名称为email_by_id()，参数传入emp_id，该函数查询emp_id的email，并返回，数据类型为字符串型。
+
+# 创建函数签执行此语句，保证函数的创建会成功
+# 2) SET GLOBAL log_bin_trust_function_creators = 1;
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+DELIMITER //
+
+-- CREATE FUNCTION email_by_id(IN emp_id INT) # error, 此时不可以写IN，因为FUNCTION中只可以为IN类型
+CREATE FUNCTION email_by_id(emp_id INT) # 此时不可以写IN，因为FUNCTION中只可以为IN类型
+RETURNS VARCHAR(25)
+BEGIN
+	RETURN (SELECT email FROM employees WHERE employee_id = emp_id);
+
+END //
+
+DELIMITER ;
+
+# 调用
+SELECT email_by_id('100');
+SELECT email_by_id(100);
+SELECT email_by_id(101);
 
 
+# 举例3：创建存储函数count_by_id()，参数传入dept_id，该函数查询dept_id部门的员工人数，并返回，数据类型为整型。
+DELIMITER //
+CREATE FUNCTION count_by_id(dept_id INT)
+RETURNS INT
+# 因为举例2中已经调用过SET GLOBAL log_bin_trust_function_creators = 1;，因此这里可以直接创建成功
+-- 			  DETERMINISTIC
+-- 				CONTAINS SQL
+-- 				READS SQL DATA
+BEGIN
+	RETURN (SELECT count(*) FROM employees WHERE department_id = dept_id);
 
+END //
+DELIMITER ;
 
+SELECT count_by_id(50);
 
 
 
