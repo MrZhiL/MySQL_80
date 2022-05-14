4afb4d759759e25eb6086e3e5d074237d9858821
Mrzhi <zhilx1997@sina.com>
2022-05-14 20:08:32 +0800

    添加关于mysql8.0 的一些新特性：窗口函数、公用表表达式和一些新特性，以及相应的练习测试

--- 01_基础篇/第十八章_MySQL的一些新特性.sql
+++ 01_基础篇/第十八章_MySQL的一些新特性.sql
@@ -519,3 +519,29 @@ WHERE employee_id NOT IN (
 
 # 或者使用for循环
 
+
+
+## 4. 练习
+CREATE TABLE students(
+id INT PRIMARY KEY AUTO_INCREMENT,
+student VARCHAR(15),
+points TINYINT
+);
+
+#2). 向表中添加数据如下
+INSERT INTO students(student,points)
+VALUES
+('张三',89),
+('李四',77),
+('王五',88),
+('赵六',90),
+('孙七',90),
+('周八',88);
+
+#3). 分别使用RANK()、DENSE_RANK() 和 ROW_NUMBER()函数对学生成绩降序排列情况进行显示
+SELECT student,points,
+RANK() OVER w AS 排序1,
+DENSE_RANK() OVER w AS 排序2,
+ROW_NUMBER() OVER w AS 排序3
+FROM students
+WINDOW w AS (ORDER BY points DESC);
